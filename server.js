5fde409775241c1cbb7aeec1a35ae579787078e5
kjatisak <devfsoft06@gmail.com>
2022-01-25 17:46:31 +0700

    Update

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const { initializeApp, applicationDefault, cert } = require("firebase-admin/app"
 const { getFirestore, Timestamp, FieldValue } = require("firebase-admin/firestore");
 const { getMessaging } = require("firebase-admin/messaging");
 const serviceAccount = require("./serviceAccountKey.json");
-// const _uri = "http://localhost:4002/expo-test-336102/us-central1/app/api/v1/";
-const _uri = "https://us-central1-expo-test-336102.cloudfunctions.net/app/api/v1/";
+const _uri = "http://localhost:5001/fir-coinstocknews/us-central1/app/api/v1/";
+// const _uri = "https://us-central1-expo-test-336102.cloudfunctions.net/app/api/v1/";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -51,8 +51,8 @@ app.post("/", (req, res) => {
   res.send("Thank you for subscribing");
 });
 
-app.get("/webview-config-url", function (req, res) {
-  console.log("webview-config-url");
+app.get("/webview-config-uri", function (req, res) {
+  console.log("webview-config-uri");
   // res.send("https://coinmarketcap.com/");
   // res.send("https://www.binance.com/vi/trade/BTC_USDT");
   res.send("http://localhost:3003");
@@ -120,6 +120,50 @@ app.post("/push-notify", async (req, res) => {
   }
 });
 
+app.post("/update-token-data", async (req, res) => {
+  console.log("==================================== update-token-data: req.body");
+  console.log(req.body);
+  console.log("====================================");
+  try {
+    axios
+      .post(_uri + "update-token-data", req.body)
+      .then((response) => {
+        console.log("==================================== update-token-data: response");
+        console.log(response);
+        console.log("====================================");
+
+        if (response && response.status === 200) {
+          res.send({
+            status: true,
+            data: response.data,
+          });
+        } else {
+          res.send({
+            status: false,
+            data: {},
+          });
+        }
+      })
+      .catch((err) => {
+        res.send({
+          status: false,
+          data: {},
+          msg: err.toString(),
+        });
+      });
+  } catch (e) {
+    console.log("==================================== update-token-data: catch");
+    console.log(e);
+    console.log("====================================");
+
+    res.send({
+      status: false,
+      data: {},
+      msg: e.toString(),
+    });
+  }
+});
+
 app.post("/update-notify-data", async (req, res) => {
   console.log("==================================== update-notify-data: req.body");
   console.log(req.body);
@@ -153,50 +197,6 @@ app.post("/update-notify-data", async (req, res) => {
       });
   } catch (e) {
     console.log("==================================== update-notify-data: catch");
-    console.log(e);
-    console.log("====================================");
-
-    res.send({
-      status: false,
-      data: {},
-      msg: e.toString(),
-    });
-  }
-});
-
-app.post("/update-notify-status", async (req, res) => {
-  console.log("==================================== update-notify-status: req.body");
-  console.log(req.body);
-  console.log("====================================");
-  try {
-    axios
-      .post(_uri + "update-notify-status", req.body)
-      .then((response) => {
-        console.log("==================================== update-notify-status: response");
-        console.log(response);
-        console.log("====================================");
-
-        if (response && response.status === 200) {
-          res.send({
-            status: true,
-            data: response.data,
-          });
-        } else {
-          res.send({
-            status: false,
-            data: {},
-          });
-        }
-      })
-      .catch((err) => {
-        res.send({
-          status: false,
-          data: {},
-          msg: err.toString(),
-        });
-      });
-  } catch (e) {
-    console.log("==================================== update-notify-status: catch");
     console.log(e);
     console.log("====================================");
 
